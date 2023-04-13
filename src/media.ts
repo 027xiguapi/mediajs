aea6c9e921799e9b27d9384efea1159b176ad8f0
fzk <458813868@qq.com>
2023-04-13 11:52:21 +0800

    +

--- src/media.ts
+++ src/media.ts
@@ -529,20 +529,44 @@ class Media extends EventTarget {
 	}
 
 	getSupportedMimeTypes() {
-		const possibleTypes = [
-			"video/webm",
-			"audio/webm",
-			"video/webm;codecs=vp9",
-			"video/webm;codecs=vp8",
-			"video/webm;codecs=daala",
-			"video/webm;codecs=h264",
-			"audio/webm;codecs=opus",
-			"video/mp4;codecs=h264",
+		const media = this.mediaType === "audio" ? "audio" : "video";
+		const types = [
+			"webm",
+			"mp4",
+			"ogg",
+			"mov",
+			"avi",
+			"wmv",
+			"flv",
+			"mkv",
+			"ts",
+			"x-matroska",
 		];
-
-		return possibleTypes.filter((mimeType) => {
-			return this._isTypeSupported(mimeType);
+		const codecs = [
+			"vp9",
+			"vp9.0",
+			"vp8",
+			"vp8.0",
+			"avc1",
+			"av1",
+			"h265",
+			"h264",
+		];
+		const supported: string[] = [];
+		const isSupported = MediaRecorder.isTypeSupported;
+		types.forEach((type: string) => {
+			const mimeType = `${media}/${type}`;
+			codecs.forEach((codec: string) =>
+				[
+					`${mimeType};codecs=${codec}`,
+					`${mimeType};codecs=${codec.toUpperCase()}`,
+				].forEach((variation) => {
+					if (isSupported(variation)) supported.push(variation);
+				}),
+			);
+			if (isSupported(mimeType)) supported.push(mimeType);
 		});
+		return supported;
 	}
 
 	_isTypeSupported(mimeType: string): boolean {
